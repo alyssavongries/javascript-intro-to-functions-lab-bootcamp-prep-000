17d97d954201904982564d7e06e73f8f97117a3d
Alyssa Vongries <alyssavongries@gmail.com>
2018-07-02 01:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,6 @@ function sayHiToGrandma(string){
     return `I can't hear you!`}
     if(string === string.toUpperCase()) {
       return `YES INDEED`}
-    }
+      if(string === `I love you`) {
+        return `I love you, too.`}
 }
